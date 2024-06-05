c15d0d654970fad81a04ce45e9914601e1db48fc
someone5678 <someone5678@users.noreply.github.com>
2024-06-05 12:21:08 +0900

    config: props: Set default wifi country code
    * To 00
    * Make it overridable
    
    Change-Id: I2025fc927363baf547b36ecd6ebe9c7fbc4d7611

--- config/common.mk
+++ config/common.mk
@@ -195,8 +195,12 @@ PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
 
 # Storage manager
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.storage_manager.enabled=true
+
+# Default wifi country code
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.boot.wificountrycode?=00
 
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
